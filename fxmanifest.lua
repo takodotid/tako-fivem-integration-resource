5fc6692629f8e63a62fd31a208997342af41bd8a
Rafly Maulana <mraflymaulana@gmail.com>
2025-09-27 20:34:52 +0700

    feat(package): adjust package

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,13 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Tako <developer@tako.id>'
-description 'Tako\'s FiveM integration module'
+description 'Tako FiveM integration module'
 version '1.0.0'
 
+server_only 'yes'
+node_version '22'
+
 server_scripts {
-  'server/main.js'
+  'server/main.js',
+  'server/hooks.js'
 }
